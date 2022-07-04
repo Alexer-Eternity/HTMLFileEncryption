2e5b6e3114a96f59ee98f899e1d3404c9ea5eb45
Alexer-Eternity <96809548+Alexer-Eternity@users.noreply.github.com>
2022-07-04 20:02:40 +1000

    Update decryption.go

--- Backend/decryption.go
+++ Backend/decryption.go
@@ -4,9 +4,13 @@ import (
  "crypto/cipher"
  "encoding/base64"
  "fmt"
+ 
 )
 var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
 const MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
+
+
+
 func Decode(s string) []byte {
  data, err := base64.StdEncoding.DecodeString(s)
  if err != nil {
@@ -14,11 +18,15 @@ func Decode(s string) []byte {
  }
  return data
 }
+
+
 func Decrypt(text, MySecret string) (string, error) {
  block, err := aes.NewCipher([]byte(MySecret))
  if err != nil {
   return "", err
  }
+ 
+ 
  cipherText := Decode(text)
  cfb := cipher.NewCFBDecrypter(block, bytes)
  plainText := make([]byte, len(cipherText))
