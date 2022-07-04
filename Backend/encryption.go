7747af0abb41dabddece6d1e919908492604a854
Alexer-Eternity <96809548+Alexer-Eternity@users.noreply.github.com>
2022-07-04 20:01:53 +1000

    Update encryption.go

--- Backend/encryption.go
+++ Backend/encryption.go
@@ -11,14 +11,13 @@ import (
 )
 
 var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
-// This should be in an env file in production
+
 const MySecret string = "abc&1*~#^2^#s0^=)^^7%b34"
 func Encode(b []byte) string {
  return base64.StdEncoding.EncodeToString(b)
 }
 
 
-// Encrypt method is to encrypt or hide any classified text
 func Encrypt(text, MySecret string) (string, error) {
  block, err := aes.NewCipher([]byte(MySecret))
  if err != nil {
@@ -52,9 +51,8 @@ return "data.txt"
 
 }
 func main() {
-    StringToEncrypt := "sadjdshafldshjigfsihdiadshfidfsahdsfi"
+    StringToEncrypt := "???"
 
-    // To encrypt the StringToEncrypt
     encText, err := Encrypt(StringToEncrypt, MySecret)
     if err != nil {
      fmt.Println("error encrypting your classified text: ", err)
